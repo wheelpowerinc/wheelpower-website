e31a1585eec1a65916c04c67cd6c6b6c9f78abb6
wheelpowerinc <wheelpower.inc@gmail.com>
2026-01-12 00:50:10 +0800

    added constant tires js

---
+++ src/lib/brands.js
@@ -0,0 +1,13 @@
+// Tire Brands Configuration
+// Update this list when you add/remove brands in Directus dropdown
+
+export const tireBrands = [
+  'Sailun',
+  'Radar',
+  'Comforser',
+  'Thunderer',
+  'Apollo',
+  'Toyo',
+  'Dunlop',
+  'Bridgestone'
+];
